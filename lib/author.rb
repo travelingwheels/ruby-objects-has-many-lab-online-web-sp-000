d352b7cb0c6c9b788fe5fcab0e10c753f56bfddb
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-03 14:02:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,12 +2,10 @@ class Author
   
   attr_accessor :name, :posts
   @@post_count = 0 
-  @@all = []
   
   def initialize(name)
     @name = name
     @posts = []
-    @@all << posts
   end
   
   def add_post(post)
